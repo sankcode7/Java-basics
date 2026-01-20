a31d489aef10126a0d0e9c90a068e81d4a61907b
sankcode7 <vsankalp875@gmail.com>
2026-01-20 20:17:52 +0530

    adding ps.java

---
+++ ps.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+
+public class ps {
+    public static void main(String []args){
+       /* int a = 56;
+        int b = 1;
+        int c = 3;
+        int sum = a + b + c;
+        System.out.println(sum);*/
+
+       /* System.out.println("marks obtained in three subjects:");
+        System.out.println("subject1 :");
+        int maths = 68;*/
+     /*   System.out.println("Enter:");
+        Scanner dc = new Scanner(System.in);
+        int a = dc.nextInt();
+        int b = 8;
+        int c = a + b;
+        System.out.println(c);*/
+
+        Scanner ac = new Scanner(System.in);
+        int a = ac.nextInt();
+        System.out.println(a>=7);
+
+
+
+
+
+}
+}
