0f18f6586f277912d35445449f77ca7e2f3b220d
sankcode7 <vsankalp875@gmail.com>
2026-01-20 19:18:27 +0530

    new file add strings.java

---
+++ Operators.java
@@ -0,0 +1,56 @@
+package Java-basics;
+
+public class Operators {
+    public static void main(String [] args) {
+        //int a = 56;
+        /*increment and decrement
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(--a);
+        System.out.println(a);
+        System.out.println(a--);
+        System.out.println(a);8*/
+
+        /*first it will increment and then it will multiply
+        int y = 7;
+        int x = ++y*8;
+        System.out.println(x);*/
+
+       /* float a = 7/4 * 9/2;
+        System.out.println(a);*/
+
+        int a = 10, b = 5;
+        double x = 10.5, y = 4.2;
+
+        // Addition
+//        System.out.println("Addition (int): " + (a + b));  // Output: 15
+//        System.out.println("Addition (double): " + (x + y));  // Output: 14.7
+
+//        // Subtraction
+//        System.out.println("Subtraction (int): " + (a - b));  // Output: 5
+//        System.out.println("Subtraction (double): " + (x - y));  // Output: 6.3
+//
+//        // Multiplication
+//        System.out.println("Multiplication (int): " + (a * b));  // Output: 50
+//        System.out.println("Multiplication (double): " + (x * y));  // Output: 44.1
+//
+//        // Division
+//        System.out.println("Division (int): " + (a / b));  // Output: 2 (integer division)
+//        System.out.println("Division (double): " + (x / y));  // Output: 2.5
+//
+//        // Modulus
+//        System.out.println("Modulus (int): " + (a % b));  // Output: 0
+//       System.out.println("Modulus (double): " + (x % y));  // Output: 2.1
+
+
+
+
+
+    }
+
+    
+    
+    
+}
---
+++ Strings.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String []args){
+       String name = "sankalp";
+      // String name = new String("sankalp");
+      //  String name = new String("sankalp");
+        System.out.print("The name is: ");
+       System.out.println(name);
+  //      int a = 6;
+//     float b = 5.6454f;
+//        System.out.printf("a is %d and b is %f" ,a,b);
+//        System.out.format("a is %d and b is %f" ,a,b);
+        // both are same
+       //Scanner a = new Scanner(System.in);
+       // String st = a.next();
+       // String st = a.nextLine();
+       // System.out.print(st);
+
+        // string methods
+       String name1 = "Sankalp   l  aw";
+//        int value = name1.length();
+//        System.out.println(value);
+
+//        String lstring = name1.toLowerCase();
+//        System.out.println(lstring);
+
+
+     //   String ustring = name1.toUpperCase();
+     //   System.out.println(ustring);
+
+//        String ustring = name1.toUpperCase();
+//        System.out.println(ustring);
+
+       //String nts = "    world     ";
+       // System.out.println(nts.trim());
+      //   String  ts = nts.trim();
+      //  System.out.println(ts);
+        // can do by both methods
+
+//      System.out.println(name1.substring(2));
+//        System.out.println(name1.substring(1,4));
+
+
+//      System.out.println(name1.startsWith("San"));
+//        System.out.println(name1.endsWith("San"));
+
+//       System.out.println(name1.replace("San","sun"));
+
+     //   System.out.println(name1.charAt(1));
+      //  System.out.println(name1.indexOf("k"));
+        System.out.println(name1.indexOf("r"));
+//        if any character is not present and we gave random char to find out the index of it will give -1
+
+        //problem 2 of practice set3
+     //  String text = "repair van";
+     //   System.out.println(text.replace(" ", "_"));
+
+      //problem3
+     //   String letter = "Dear <|name|> , thanks a lot ";
+     //   letter = letter.replace("<|name|>","krishna");
+     //   System.out.println(letter);
+
+      /*  String space = "this string contains      double and triple spaces";
+        System.out.println(space.indexOf("  "));
+        System.out.println(space.indexOf("   "));
+
+        String flow = "Dear name, \n this java course is nice.\n thanks";
+        System.out.println(flow);*/
+
+//        escape sequence characters after backslash = escape sequence characters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
