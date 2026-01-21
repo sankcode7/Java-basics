a3e1438370c010aa35e9288984dc48676ff6d8df
sankcode7 <vsankalp875@gmail.com>
2026-01-22 00:47:21 +0530

    new file add

---
+++ conditionals.java
@@ -0,0 +1,34 @@
+public class conditionals {
+    public static void main(String [] args){
+      /*  int age = 5;
+        if(age>18){
+            System.out.println("yes boy you can drive!");
+        }
+        else{
+            System.out.println("No boy you cannot drive yet!");
+            }*/
+
+
+      /* int age = 17;
+        if(age==18){
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("no");
+        }*/
+
+
+
+       int age =18;
+        if(age!=18){
+            System.out.println("hello boy");
+        }
+        else{
+            System.out.println("oh yes");
+        }
+
+
+        }
+    }
+
+
