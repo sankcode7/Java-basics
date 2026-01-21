f8fe1582ccfc1cbd255a84d28596b38ad12adb0e
sankcode7 <vsankalp875@gmail.com>
2026-01-22 02:26:50 +0530

    adding files

---
+++ break_and_continue_statement.java
@@ -0,0 +1,40 @@
+public class break_and_continue_statement {
+    public static void main(String[] args) {
+//       for (int i = 0; i < 5; i++) {
+//
+//
+//           if (i == 2) {
+//               System.out.println("Ending the loop");
+//                continue;
+//            }
+//                System.out.println(i);
+//                System.out.println("Java is great");
+//
+//        }
+
+
+
+        int i = 0;
+        while(i<5) {
+            if (i == 2) {
+                System.out.println("Ending the loop");
+                continue;
+            }
+            System.out.println(i);
+            System.out.println("Java is great");
+
+    }
+//      int i=0;
+//     do{
+//         System.out.println(i);
+//         System.out.println("Java is great");
+//         if(i==2){
+//             System.out.println("Ending the loop");
+//             break;
+//         }
+//         i++;
+//      }while(i<5);
+//        System.out.println("loop ends here");
+                //           }
+            }
+        }
---
+++ elseif.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class elseif {
+    public static void main(String [] args) {
+
+      /*  int age;
+        System.out.println("enter your age");
+        Scanner sc = new Scanner(System.in);
+        age = sc.nextInt();
+        switch(age){
+            case 18:
+                System.out.println("You are going to become an adult");
+                break;
+            case 23:
+                System.out.println("You are going to join a Job!");
+                break;
+            case 60:
+                System.out.println("You are going to get retired");
+                break;
+            default:
+                System.out.println("Enjoy your life!");
+        }
+        System.out.println("Thanks for using my java code!");*/
+
+        int age;
+        System.out.println("enter your age");
+        Scanner sc = new Scanner(System.in);
+        age = sc.nextInt();
+        switch(age){
+            case 18:
+                System.out.println("You are going to become an adult");
+                break;
+            case 23:
+                System.out.println("You are going to join a job!");
+                break;
+            case 60:
+                System.out.println("You are going to get retired");
+                break;
+            default:
+                System.out.println("Enjoy your life!");
+        }
+        System.out.println("Thanks for using my java code!");
+
+
+
+//        int age;
+//        System.out.println("enter your age");
+//        Scanner sc = new Scanner(System.in);
+//        age = sc.nextInt();
+//        switch(age) {
+//            case 18:
+//                System.out.println("You are going to become an adult");
+//                break;
+//            case 23:
+//                System.out.println("You are going to join a job");
+//                break;
+//            case 60:
+//                System.out.println("You are going to get retired");
+//                break;
+//            default:
+//                System.out.println("Enjoy your life");
+//        }
+//        System.out.println("Thanks for using my java code!");
+//
+//         if (age>56){
+//             System.out.println("You are experienced");
+//         }
+//         else if(age>46){
+//            System.out.println("you are semi experienced");
+//        }
+//         else if(age>=36){
+//             System.out.println("you are semi semi experienced");
+//         }
+//         else{
+//            System.out.println("You are not experienced");
+//        }
+//
+//
+//        // if elseif elseif else ladder
+//       /* if(age>56){
+//            System.out.println("you are experienced");
+//        }
+//        else if(age>46){
+//            System.out.println("you are semi-experienced");
+//        } else if (age>=36) {
+//            System.out.println("you are semi-semi-experienced");
+//        }
+//        else{
+//            System.out.println("you are not experienced");
+//        }*/
+    }
+}
