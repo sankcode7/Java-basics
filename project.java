b5e4cdff456f6d7fb5df849ecc7f60c4ba55b9d5
sankcode7 <vsankalp875@gmail.com>
2026-01-22 16:12:18 +0530

    add practice.javafile

---
+++ project.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class project {
+    public static void main(String [] args){
+        /*Scanner scan = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = scan.next();
+        System.out.println("Hello " + name + " have a good day!");*/
+
+       // System.out.println("Enter your name");
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        System.out.println(c>9);
+
+
+
+       /* System.out.println(sc);
+        int a = 455;
+        Scanner sa = new Scanner(System.in);
+        System.out.println("enter no");
+        int b = sa.nextInt();
+         boolean c = b >= a;
+        System.out.println(c);*/
+
+        /*int x = 7;
+        int a = (7*49/7 + 35/7);
+        System.out.println(a);*/
+
+
+
+    }
+}
