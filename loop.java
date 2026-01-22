47cc84bfefec7fc7f92067d985bcdfcbef3585c4
sankcode7 <vsankalp875@gmail.com>
2026-01-22 16:08:50 +0530

    add loop file

---
+++ loop.java
@@ -0,0 +1,108 @@
+public class loop {
+    public static void main(String [] args){
+     //System.out.println(1);
+     //   System.out.println(2);
+      //  System.out.println(3);
+
+
+       // using while loop
+       /* int i=1;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("finish running while loop!");*/
+
+//        int i=1;
+//        while(i<=3){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//       int j =1;
+//        while(j<7){
+//            System.out.println(j);
+//            j++;
+//        }
+
+        // while loop practicing syntax
+        //  int age1;
+        //age1 = 4;
+        // OR
+//        int age1 = 4;
+//        while(age1<10){
+//            System.out.println(age1);
+//            age1++;
+//        }
+        // it will run infinitely
+        // boolean will run
+//       while(true){
+//             System.out.println("hello java");
+
+
+        // do while loop
+//        int b = 0;
+//        do {
+//            System.out.println(b);
+//            b++;
+//        }while(b<5);
+
+//        int c =1;
+//        do{
+//            System.out.println(c);
+//            c++;
+//        }while(c<=35);
+
+       /*  int b=4;
+         do{
+            System.out.println(b);
+            b++;
+        }while(b<32);*/
+
+        // for loop
+     /*   for(int i=1; 1<=10; i++){
+            System.out.println(i);*/
+      //  }
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+
+
+        //quick quiz
+      //  Write a program to print first n natural no.s using do-while loop.
+        // while loop
+      /*  int a = 100;
+        while(a<=200){
+            System.out.println(a);
+            a++;
+        }*/
+        // do while loop
+//        int d= 100;
+//        do{
+//            System.out.println(d);
+//            d++;
+//        }
+//        while(d<=200);
+
+
+//       for(initialize;check expression ; update)
+//        // write a program to print first n natural numbers using do while loop.
+//        int n=1;
+//        do{
+//            System.out.println(n);
+//            n++;
+//        }
+//        while(n<10);
+
+//        decrementing for loop
+//        for(i=7;i!=0;i--){
+//        System.out.println(i);
+//        }
+
+
+
+
+
+
+    }
+}
