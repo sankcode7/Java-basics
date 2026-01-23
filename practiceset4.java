4d7631fbeab94ed38196ffa2615aa68a36de6a33
sankcode7 <vsankalp875@gmail.com>
2026-01-23 23:58:40 +0530

    add new file

---
+++ practiceset4.java
@@ -0,0 +1,68 @@
+l import java.util.Scanner;
+
+
+public class practiceset4 {
+    public static void main(String [] args ){
+        // problem1
+      /*  int a = 10;
+        if (a == 11){
+            System.out.println("I am 11");
+        }
+        else{
+            System.out.println("I am not 11");
+        }*/
+     /*   byte marks1, marks2, marks3 ;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your marks in physics");
+        marks1 = sc.nextByte();
+        System.out.println(marks1);
+        System.out.println("enter your marks in chemistry");
+        marks2 = sc.nextByte();
+        System.out.println(marks2);
+        System.out.println("enter your marks in maths");
+        marks3 = sc.nextByte();
+        System.out.println(marks3);
+
+        float avg = marks1+marks2+marks3/3.0f;
+        System.out.println("your overall percentage is"+ avg);
+        if(avg>=40 && marks1>=33 && marks2>=33 && marks3>=33){
+            System.out.println("Congratulations you have been promoted");
+        }
+        else{
+            System.out.println("Sorry you have been not promoted!");
+        }*/
+
+       /* Scanner sc = new Scanner(System.in);
+        System.out.println("Enter kilometer");
+        int n = sc.nextInt();
+        double convert = n/1.609;
+        System.out.println(convert);*/
+
+        System.out.println("Enter your number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+
+
+
+
+
+
+
+
+
+
+
+        //String name = sc.next();
+
+      //  System.out.println("Hello "+ name +" have a good day");
+
+
+
+       }
+
+
+
+
+
+    }
+
