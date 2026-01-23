abbc9be2b1fe503cff210b7c0989b2b9f35ed4b7
sankcode7 <vsankalp875@gmail.com>
2026-01-23 16:28:08 +0530

    adding for loop .java file

---
+++ for_loop.java
@@ -0,0 +1,28 @@
+public class for_loop {
+    public static void main(String[] args) {
+//        for(int a=1;a<=4;a++){
+//        System.out.println(a);
+//        }
+
+//        2i= Even numbers = 0,2,4,6,8
+//        2i+1 = Odd numbers = 1,3,5,7,9
+//        write a program to print first n odd numbers using a for loop.
+//
+//   decrementing for loop
+//        for(int i=5;i>0;i--){
+//            System.out.println(i);}
+//            OR
+//            for (int i=5;i!=0;i--){
+//                System.out.println(i);
+//    }
+
+
+
+        int n= 100;
+        for (int j=n; j!=0;j--){
+                    System.out.println(j);
+
+
+        }
+    }
+}
