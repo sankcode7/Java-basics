6716e7d7fc96e2d7ec55d808b753c2d224cc2219
sankcode7 <vsankalp875@gmail.com>
2026-01-29 21:59:26 +0530

    adding practiceset6.file

---
+++ practiceset_6.java
@@ -0,0 +1,79 @@
+public class practiceset_6 {
+    public static void main(String[] args) {
+//        float [] marks = {91.1f, 92.1f,93.7f,94.3f,96.1f};
+//        float sum =0;
+//        for(float element:marks){
+//            sum =  sum + element;
+//        }
+//        System.out.println("the value of sum is " + sum);
+
+//        repeated for practicing
+//        float [] marks = {91.5f,92.1f,93.7f,94.3f,96.1f};
+//       float sum =0;
+//       for(float element : marks){
+//           sum = sum + element;
+//       }
+//        System.out.println("the value of sum is " + sum);
+
+
+
+    //  pratice problem 2
+//        float [] marks = {91.1f, 92.1f,93.7f,94.3f,96.1f};
+//        float num =92.1f;
+//        boolean isInArray = false;
+//        for(float element:marks){
+//         if(num==element){
+//             isInArray =true;
+//         }
+//        }
+//        System.out.println("the value of sum is " + num);
+
+//        float [] marks = {91.1f, 92.1f,93.7f,94.3f,96.1f};
+//        float num = 93.1f;
+//        boolean isInArray = false;
+//        for(float element: marks){
+//            if(num==element){
+//                isInArray = true;
+//                break;
+//            }
+//        }
+//        if(isInArray){
+//            System.out.println("The value is present in the array");
+//        }else{
+//            System.out.println("The value is not present in the array");
+        };
+
+//        practice problem 3
+
+//        float [] marks1 = {91.5f,92.1f,93.7f,94.3f,96.1f};
+//       float sum =0;
+//       for(float element : marks1){
+//           sum = sum + element;
+//      }
+//        System.out.println("the value of average marks is " + sum/marks.length);
+
+//       practice problem 4
+        int [][] mat1 ={{1,2,3},
+                       {4,5,6}};
+
+        int [][] mat2 ={{2,6,13 },
+                       {3,7,1}};
+
+        int [][] result ={{0,0,0 },
+                          {0,0,0}};
+
+//        for ( int i =0;i<mat1.length ; i++){
+//            for(int j=0;j<mat1[j].length;j++){
+//                System.out.format("Setting value for i=%d and j = %d\n ", i, j );
+//               result[i][j] = mat1[i][j] + mat2[i][j];
+
+            }
+ //       }
+
+
+
+
+
+
+//    }
+//}
